b235a31797e0b83439676e524d0dcfe0f6bdc6fd
Thomas Mueller <thomas.tom.mueller@gmail.com>
2018-11-30 18:16:16 +0100

    Fixed a bug in radix sort

--- src/main/java/org/fastfilter/gcs/Sort.java
+++ src/main/java/org/fastfilter/gcs/Sort.java
@@ -10,23 +10,23 @@ public class Sort {
 
     public static void sortUnsigned(long[] data, int offset, int len) {
         int[] histogram = new int[257];
-        int shift = 0;
-        long mask = 0xFF;
-        long[] buffer = new long[Math.min(len, data.length)];
-        while (shift < Long.SIZE) {
+        len = Math.min(len, data.length - offset);
+        long[] buffer = new long[len];
+        for(int shift = 0; shift < Long.SIZE; shift += 8) {
             Arrays.fill(histogram, 0);
-            for (int i = offset; i + offset < buffer.length; ++i) {
-                ++histogram[(int)((data[i] & mask) >>> shift) + 1];
+            for (int i = 0; i < len; i++) {
+                int b = (int)((data[i + offset] >>> shift) & 0xff);
+                histogram[b + 1]++;
             }
-            for (int i = 0; i + 1 < histogram.length; ++i) {
+            for (int i = 0; i + 1 < histogram.length; i++) {
                 histogram[i + 1] += histogram[i];
             }
-            for (int i = offset; i + offset < buffer.length; ++i) {
-                buffer[histogram[(int)((data[i] & mask) >>> shift)]++] = data[i];
+            for (int i = 0; i < len; i++) {
+                int b = (int)((data[i + offset] >>> shift) & 0xff);
+                int index = histogram[b]++;
+                buffer[index] = data[i + offset];
             }
-            System.arraycopy(buffer, 0, data, offset, buffer.length);
-            shift += 8;
-            mask <<= 8;
+            System.arraycopy(buffer, 0, data, offset, len);
         }
     }
 
