87a56473871a91c64f223780d1302ff2cbbd787e
Richard Startin <richard@openkappa.co.uk>
2019-12-27 15:00:45 +0000

    revert change to XorSimple, add false negative test cases for MPHF, GCS2, COUNTING_BLOOM

--- fastfilter/src/test/java/org/fastfilter/SimpleFuzzer.java
+++ fastfilter/src/test/java/org/fastfilter/SimpleFuzzer.java
@@ -8,32 +8,29 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.LongStream;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.fastfilter.FilterType.*;
 
 public class SimpleFuzzer {
 
-    // implementations with bugs which may not be worth fixing
-    private static final EnumSet<FilterType> IGNORED = EnumSet.of(FilterType.GCS2, FilterType.MPHF);
-
     public static void main(String... args) {
         long seed = 0;
-        for (int keyLength = 3; keyLength < 1_000_000; keyLength += 100) {
-            long[] keys = LongStream.range(0, keyLength).map(i -> ThreadLocalRandom.current().nextLong()).toArray();
-            for (FilterType type : FilterType.values()) {
-                if (IGNORED.contains(type)) {
-                    continue;
-                }
-                try {
-                    for (int i = 0; i < 1_000; ++i) {
-                        seed = ThreadLocalRandom.current().nextLong();
-                        Hash.setSeed(seed);
-                        Filter filter = type.construct(keys, 8);
-                        for (long key : keys) {
-                            assertTrue(seed + "/" + type + "/" + Arrays.toString(keys), filter.mayContain(key));
+        for (int bitsPerKey = 8; bitsPerKey < 32; bitsPerKey += 8) {
+            for (int keyLength = 3; keyLength < 1_000_000; keyLength += ThreadLocalRandom.current().nextInt(10000)) {
+                long[] keys = LongStream.range(0, keyLength).map(i -> ThreadLocalRandom.current().nextLong()).toArray();
+                for (FilterType type : FilterType.values()) {
+                    try {
+                        for (int i = 0; i < 1_000_000; ++i) {
+                            seed = ThreadLocalRandom.current().nextLong();
+                            Hash.setSeed(seed);
+                            Filter filter = type.construct(keys, bitsPerKey);
+                            for (long key : keys) {
+                                assertTrue(seed + "/" + type + "/" + Arrays.toString(keys), filter.mayContain(key));
+                            }
                         }
+                    } catch (Exception e) {
+                        System.out.println(seed + "/" + type + "/" + Arrays.toString(keys));
+                        throw e;
                     }
-                } catch (Exception e) {
-                    System.out.println(seed + "/" + type + "/" + Arrays.toString(keys));
-                    throw e;
                 }
             }
         }
