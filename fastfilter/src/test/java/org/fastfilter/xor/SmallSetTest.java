a094c0a0ccf73c5894352ff154dfd5942a6b72ed
Thomas Mueller <thomasm@apache.org>
2021-12-23 11:26:49 +0100

    XorBinaryFuse8 failed to construct on some data, at around 11511 entries #32

--- fastfilter/src/test/java/org/fastfilter/xor/SmallSetTest.java
+++ fastfilter/src/test/java/org/fastfilter/xor/SmallSetTest.java
@@ -16,4 +16,37 @@ public class SmallSetTest {
         XorSimple.construct(new long[]{0xef9bddc5166c081cL, 0x33bf87adaa46dcfcL});
         XorSimple2.construct(new long[]{0xef9bddc5166c081cL, 0x33bf87adaa46dcfcL});
     }
+    
+    @Test
+    public void verySmallSizes() {
+        int n = 1;
+        for (; n < 2000; n++) {
+            testWithSize(n);
+        }
+        for (; n < 20000; n += 7) {
+            testWithSize(n);
+        }
+    }
+        
+    @Test
+    public void smallSizes() {
+        long lastTime = System.currentTimeMillis();
+        for (int n = 1; n < 1_500_000; n = (int) ((n * 1.01) + 7)) {
+            XorBinaryFuse8 f = testWithSize(n);
+            long now = System.currentTimeMillis();
+            if (now - lastTime > 5000) {
+                lastTime = now;
+                System.out.println("n=" + n + " " + f.toString());
+            }
+        }
+    }
+    
+    private static XorBinaryFuse8 testWithSize(int n) {
+        long[] keys = new long[n];
+        for (int i = 0; i < n; i++) {
+            keys[i] = i;
+        }
+        return XorBinaryFuse8.construct(keys);
+    }
+
 }
